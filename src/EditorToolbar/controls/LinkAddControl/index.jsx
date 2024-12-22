b9071a5d6bda5d381253334bbeda7de936314112
Het <hetbhara@ualberta.ca>
2024-12-21 18:34:18 -0700

    Add the error for textField in case of error

--- src/EditorToolbar/controls/LinkAddControl/index.jsx
+++ src/EditorToolbar/controls/LinkAddControl/index.jsx
@@ -16,6 +16,7 @@ function LinkAddControl() {
     const editorFocus = useEditorFocus();
     const [isDialogOpened, setIsDialogOpened] = React.useState(false);
     const [link, setLink] = React.useState('');
+    const [shouldShowError, setShouldShowError] = React.useState(false);
 
     const onClick = () => {
         setLink('');
@@ -31,13 +32,18 @@ function LinkAddControl() {
     };
 
     const handleAddLink = () => {
-        if (link.trim() === '') return;
+        if (!link.trim()) {
+            setShouldShowError(true);
+            return;
+        }
 
         // If link doesn't start with "http://" or "https://", prepend "https://"
         let sanitizedLink = link;
         if (!/^https?:\/\//i.test(link)) {
             sanitizedLink = `https://${link}`;
         }
+
+        setShouldShowError(false);
 
         handleCloseDialog();
 
@@ -67,6 +73,8 @@ function LinkAddControl() {
                         value={link}
                         onChange={onChangeLink}
                         fullWidth
+                        error={shouldShowError}
+                        helperText={shouldShowError ? 'Invalid URL' : ''}
                     />
                 </DialogContent>
                 <DialogActions>
